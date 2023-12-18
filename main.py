8b28dd7bbd23328c8716992d29bc42033db1a908
Taliko <taliko.hofmann@stud-mail.uni-wuerzburg.de>
2023-12-18 14:51:02 +0100

    Funktionen richtig und plot

---
+++ main.py
@@ -0,0 +1,37 @@
+#import modules
+
+import numpy as np 
+import matplotlib as mpl 
+import matplotlib.pyplot as plt 
+from scipy import constants as cons
+
+#define some constants 
+
+me = cons.electron_mass #m_e
+hbar = cons.hbar        #reduced planck constant
+h = cons.h              #planck constant
+
+
+
+
+
+
+
+
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+def green_function(on_site_energy, hamiltonian, t)
+    
